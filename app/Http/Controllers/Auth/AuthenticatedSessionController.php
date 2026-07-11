b880b2cc708e561be21d678df05c4f5f2e09896a
Elsima09 <elsimawatiberutu@gmail.com>
2026-07-11 23:13:08 +0700

    Add role based login redirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -23,13 +23,24 @@ class AuthenticatedSessionController extends Controller
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
+{
+    $request->authenticate();
 
-        $request->session()->regenerate();
+    $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+
+    $user = Auth::user();
+
+
+    if($user->role == 'admin'){
+
+        return redirect('/admin');
+
     }
+
+
+    return redirect('/dashboard');
+}
 
     /**
      * Destroy an authenticated session.
