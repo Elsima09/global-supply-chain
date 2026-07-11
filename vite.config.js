f9fccf59c3d4666b680af3c46378cd82cd39fccb
Elsima09 <elsimawatiberutu@gmail.com>
2026-07-11 16:36:41 +0700

    fix vite production assets

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
+
+    build:{
+        manifest:true,
+    },
+
+    plugins:[
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input:[
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh:true,
         }),
     ],
+
 });
