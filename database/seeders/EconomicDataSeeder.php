7a71bffd356084e1078b2857c2797e2fb5069173
Elsima09 <elsimawatiberutu@gmail.com>
2026-07-12 19:54:13 +0700

    Optimize economic data seeder using batch WorldBank API

--- database/seeders/EconomicDataSeeder.php
+++ database/seeders/EconomicDataSeeder.php
@@ -5,13 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Country;
 use App\Models\EconomicData;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-
 
 class EconomicDataSeeder extends Seeder
 {
-
 
     public function run(): void
     {
@@ -19,43 +16,41 @@ class EconomicDataSeeder extends Seeder
 
         /*
         |--------------------------------------------------------------------------
-        | ISO 2 TO ISO 3
+        | Ambil semua data World Bank sekaligus
         |--------------------------------------------------------------------------
         */
 
 
-        $library = new \League\ISO3166\ISO3166();
+        echo "Downloading GDP...\n";
+
+        $gdp = $this->getWorldBankIndicator(
+            'NY.GDP.MKTP.CD'
+        );
 
 
-        $iso3=[];
+        echo "Downloading Inflation...\n";
+
+        $inflation = $this->getWorldBankIndicator(
+            'FP.CPI.TOTL.ZG'
+        );
 
 
-        foreach($library->all() as $item){
+        echo "Downloading Export...\n";
 
-            $iso3[$item['alpha2']] = $item['alpha3'];
-
-        }
-
-
+        $exports = $this->getWorldBankIndicator(
+            'NE.EXP.GNFS.CD'
+        );
 
 
+        echo "Downloading Import...\n";
 
-        $skipCountries=[
+        $imports = $this->getWorldBankIndicator(
+            'NE.IMP.GNFS.CD'
+        );
 
-            'HM',
-            'TF',
-            'GU',
-            'GF',
-            'GI',
-            'UM',
-            'PN',
-            'TK',
-            'NU',
-            'WF',
-            'BV'
 
-        ];
 
+        echo "Saving countries...\n";
 
 
 
@@ -63,13 +58,23 @@ class EconomicDataSeeder extends Seeder
         {
 
 
-            echo "\nProcessing {$country->name}\n";
+            $code = $country->code;
 
 
 
-            if(in_array($country->code,$skipCountries)){
+            /*
+            |--------------------------------------------------------------------------
+            | ISO 2 ke ISO 3
+            |--------------------------------------------------------------------------
+            */
 
-                echo "Skipped Territory\n";
+            $iso3 = $this->convertISO($code);
+
+
+
+            if(!$iso3){
+
+                echo "Skip {$country->name}\n";
 
                 continue;
 
@@ -77,118 +82,119 @@ class EconomicDataSeeder extends Seeder
 
 
 
-            if(!isset($iso3[$country->code])){
 
-                echo "Skipped ISO\n";
+            /*
+            |--------------------------------------------------------------------------
+            | Tahun 2020-2025
+            |--------------------------------------------------------------------------
+            */
 
-                continue;
+
+            for($year=2020;$year<=2025;$year++)
+            {
+
+
+                EconomicData::updateOrCreate(
+
+                    [
+
+                        'country_id'=>$country->id,
+
+                        'year'=>$year
+
+                    ],
+
+
+                    [
+
+                        'gdp'=>$gdp[$iso3][$year] ?? 0,
+
+                        'inflation'=>$inflation[$iso3][$year] ?? 0,
+
+                        'exports'=>$exports[$iso3][$year] ?? 0,
+
+                        'imports'=>$imports[$iso3][$year] ?? 0
+
+                    ]
+
+                );
+
 
             }
 
 
 
-            $code=$iso3[$country->code];
+            echo "Saved {$country->name}\n";
+
+
+        }
+
+
+
+        echo "Economic data finished\n";
+
+
+    }
 
 
 
 
-            try {
+
+
+    private function getWorldBankIndicator($indicator)
+    {
+
+
+        $result=[];
+
+
+        try {
+
+
+            $response = Http::timeout(60)
+            ->get(
+
+"https://api.worldbank.org/v2/country/all/indicator/{$indicator}?date=2020:2025&format=json&per_page=20000"
+
+            );
 
 
 
-                /*
-                |--------------------------------------------------------------------------
-                | GET 6 YEARS DATA
-                |--------------------------------------------------------------------------
-                */
+            $data=$response->json();
 
 
-                $gdp =
-                $this->getHistory(
-                    $code,
-                    'NY.GDP.MKTP.CD'
-                );
 
+            if(!isset($data[1])){
 
-                $inflation =
-                $this->getHistory(
-                    $code,
-                    'FP.CPI.TOTL.ZG'
-                );
+                return [];
 
-
-                $exports =
-                $this->getHistory(
-                    $code,
-                    'NE.EXP.GNFS.CD'
-                );
-
-
-                $imports =
-                $this->getHistory(
-                    $code,
-                    'NE.IMP.GNFS.CD'
-                );
+            }
 
 
 
 
-                /*
-                |--------------------------------------------------------------------------
-                | SAVE PER YEAR
-                |--------------------------------------------------------------------------
-                */
+            foreach($data[1] as $item)
+            {
 
 
-                foreach($gdp as $year=>$value)
+                if(
+                    isset($item['value'])
+                    &&
+                    $item['value'] !== null
+                )
                 {
 
 
-                    EconomicData::updateOrCreate(
-
-                        [
-
-                            'country_id'=>$country->id,
-
-                            'year'=>$year
-
-                        ],
-
-
-                        [
-
-                            'gdp'=>$value ?? 0,
-
-
-                            'inflation'=>$inflation[$year] ?? 0,
-
-
-                            'exports'=>$exports[$year] ?? 0,
-
-
-                            'imports'=>$imports[$year] ?? 0
-
-                        ]
-
-                    );
+                    $result[
+                        $item['countryiso3code']
+                    ][
+                        $item['date']
+                    ]
+                    =
+                    $item['value'];
 
 
                 }
-
-
-
-                echo "SUCCESS {$country->name}\n";
-
-
-
-            }
-
-
-
-            catch(\Throwable $e){
-
-
-                echo "FAILED {$country->name} : ".$e->getMessage()."\n";
 
 
             }
@@ -196,9 +202,17 @@ class EconomicDataSeeder extends Seeder
 
 
         }
+        catch(\Throwable $e){
 
 
-        echo "\nEconomic Seeder Finished\n";
+            echo $e->getMessage();
+
+
+        }
+
+
+
+        return $result;
 
 
     }
@@ -209,116 +223,30 @@ class EconomicDataSeeder extends Seeder
 
 
 
-
-    /*
-    |--------------------------------------------------------------------------
-    | WORLD BANK HISTORY
-    |--------------------------------------------------------------------------
-    */
-
-
-    private function getHistory($country,$indicator)
+    private function convertISO($code)
     {
 
 
-        $cacheKey =
-        "wb_history_{$country}_{$indicator}";
+        $library =
+        new \League\ISO3166\ISO3166();
 
 
 
-        return Cache::remember(
+        foreach($library->all() as $item)
+        {
 
-            $cacheKey,
+            if($item['alpha2']==$code)
+            {
 
-            now()->addHours(24),
-
-
-            function() use ($country,$indicator){
-
-
-
-                try {
-
-
-                    $response = Http::connectTimeout(5)
-                    ->timeout(15)
-                    ->get(
-
-"https://api.worldbank.org/v2/country/{$country}/indicator/{$indicator}?date=2020:2025&format=json&per_page=100"
-
-                    );
-
-
-
-                    if(!$response->successful()){
-
-                        return [];
-
-                    }
-
-
-
-                    $json=$response->json();
-
-
-
-                    if(!isset($json[1])){
-
-                        return [];
-
-                    }
-
-
-
-                    $result=[];
-
-
-
-                    foreach($json[1] as $item){
-
-
-
-                        if(
-                            isset($item['value'])
-                            &&
-                            $item['value'] !== null
-                        ){
-
-
-                            $result[
-                                $item['date']
-                            ] =
-                            $item['value'];
-
-
-                        }
-
-
-                    }
-
-
-
-                    return $result;
-
-
-
-                }
-
-
-                catch(\Throwable $e){
-
-
-                    return [];
-
-
-                }
-
-
+                return $item['alpha3'];
 
             }
 
+        }
 
-        );
+
+
+        return null;
 
 
     }
