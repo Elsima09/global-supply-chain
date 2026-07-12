1681d9651e0bbc3f50aee2d9c93a68e988c7d124
Elsima09 <elsimawatiberutu@gmail.com>
2026-07-12 20:32:57 +0700

    Fix RiskHistory fillable

--- app/Models/RiskHistory.php
+++ app/Models/RiskHistory.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class RiskHistory extends Model
 {
-    //
+
+    protected $fillable = [
+
+        'country_id',
+
+        'risk_score',
+
+        'recorded_at'
+
+    ];
+
+
+
+    public function country()
+    {
+        return $this->belongsTo(
+            Country::class
+        );
+    }
+
 }
